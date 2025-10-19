90b04c4b965027e69d353f8cda6a57778ad28c05
Jonathan Houze <jonathan.houze@gmail.com>
2025-10-19 10:21:35 +0800

    feat: improve gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,27 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+// ==============================================================================
+// Plugin
+// ==============================================================================
 
 plugins {
+    // ==============================================================================
+    // Core Framework Plugins
+    // ==============================================================================
+    alias(libs.plugins.spring.boot)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.spring)
-    alias(libs.plugins.spring.boot)
+
+    // ==============================================================================
+    // Development & Testing Plugins
+    // ==============================================================================
+    alias(libs.plugins.test.retry)
+    alias(libs.plugins.gradle.versions)
+
+    // ==============================================================================
+    // Analysis/quality
+    // ==============================================================================
+    alias(libs.plugins.dependency.analysis)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.detekt)
 }
 
 group = "com.frisboo.corebanking"
@@ -12,7 +30,7 @@ description = "Core files for core banking services"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
@@ -23,10 +41,34 @@ repositories {
 
 kotlin {
 	compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_24)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
 }
+
+spotless {
+    kotlin {
+        ktlint() // uses latest ktlint by default; pin if desired
+        target("**/*.kt")
+    }
+    kotlinGradle {
+        ktlint()
+        target("**/*.kts")
+    }
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    autoCorrect = true
+}
+
+//tasks.jacocoTestReport {
+//    reports {
+//        xml.required.set(true)
+//        html.required.set(true)
+//    }
+//}
 
 tasks.withType<Test> {
 	useJUnitPlatform()
